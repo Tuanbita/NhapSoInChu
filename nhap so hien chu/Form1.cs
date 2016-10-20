971534e0b54d3954ae05e02804576e12754cb391
Tuanbita <tuanbita@gmail.com>
2016-10-20 14:32:25 +0700

    Vẫn chưa cải tiến đc gì cả.

--- nhap so hien chu/Form1.cs
+++ nhap so hien chu/Form1.cs
@@ -21,66 +21,9 @@ namespace nhap_so_hien_chu
 
             string s = txtSo.Text;
 
-            int so = Convert.ToInt16(s);
+            int so = Convert.ToInt32(s);
 
-            switch (so)
-            {
-                case 1:
-                    {
-                        string x = "Mot";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 2:
-                    {
-                        string x = "Hai";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 3:
-                    {
-                        string x = "Ba";
-                        txtChu.Text = x;
-                        break;
-                    }
-
-                case 4:
-                    {
-                        string x = "Bon";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 5:
-                    {
-                        string x = "Nam";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 6:
-                    {
-                        string x = "Sau";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 7:
-                    {
-                        string x = "Bay";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 8:
-                    {
-                        string x = "Tam";
-                        txtChu.Text = x;
-                        break;
-                    }
-                case 9:
-                    {
-                        string x = "Chin";
-                        txtChu.Text = x;
-                        break;
-                    }
-            }
+            
         }
 
         private void txtThoat_Click(object sender, EventArgs e)
